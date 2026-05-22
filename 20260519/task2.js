7f050a42cf83eca0ffb92d691f2e0d2cf401692e
malakyan1pix-cmd <malakyan1pix@gmail.com>
2026-05-23 00:53:37 +0400

    homework 19/05

---
+++ 20260519/task2.js
@@ -0,0 +1,18 @@
+function BubbleSort(arr){
+    for(let i = 0; i <= arr.length - 1; i++){
+
+        for(let j = 0; j < arr.length - i - 1; j++){
+
+            if(arr[j] > arr[j + 1]){
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+
+    return arr;
+}
+
+
+const arr = [5, 3, 8, 4, 2];
+console.log(BubbleSort(arr));
+
