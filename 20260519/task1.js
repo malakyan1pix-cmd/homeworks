fe98da36dbeb5f2117550e344d3fae8e0d1604c3
malakyan1pix-cmd <malakyan1pix@gmail.com>
2026-05-29 18:27:53 +0400

    homework 19/05

--- 20260519/task1.js
+++ 20260519/task1.js
@@ -1,12 +1,15 @@
 function linearSearch (arr, target){
+    let steps = 0;
+
     for(let i = 0; i < arr.length; i++){
+        steps++;
 
         if(arr[i] === target){
-            return i;
+            return [i, steps];
         }
     }
-
     return -1;
+    
 }
 
 
--- 20260519/task2.js
+++ 20260519/task2.js
@@ -1,7 +1,10 @@
 function BubbleSort(arr){
+    let steps = 0;
+
     for(let i = 0; i <= arr.length - 1; i++){
 
         for(let j = 0; j < arr.length - i - 1; j++){
+            steps++;
 
             if(arr[j] > arr[j + 1]){
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
@@ -9,7 +12,7 @@ function BubbleSort(arr){
         }
     }
 
-    return arr;
+    return [arr, steps];
 }
 
 
